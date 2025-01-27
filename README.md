a101ee3f0e8cbf39c4aeded75026ae2a7219a527
pinje <melvinaustinn@gmail.com>
2025-01-27 16:03:45 +0700

    Improve: Advanced Course

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ for CORS problem change browser://flags
 | Insecure origins treated as secure                              | Enabled (You can add <br> `http://localhost,http://localhost:5000,http://notesapp-v1.dicodingacademy.com`on origin section) |
 | Allow invalid certificates for resources loaded from localhost. |                                                           Enabled                                                           |
 | Block insecure private network requests                         |                                                           Default                                                           |
+
+Run command PM2:
+
+-   Start initialization : `pm2 start npm --name "notes-api" -- run "start-prod"`
+-   Restart : `pm2 restart notes-`api`
+-   Stop : `pm2 stop notes-api`
+-   Start again : `pm2 start notes-api`
---
+++ src/services/inMemory/NotesServices.js
@@ -0,0 +1,77 @@
+const { nanoid } = require('nanoid');
+
+class NotesService {
+  constructor() {
+    this._notes = [];
+  }
+
+  // ADD NOTE
+  addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      title,
+      tags,
+      body,
+      id,
+      createdAt,
+      updatedAt,
+    };
+
+    this._notes.push(newNote);
+
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error('Catatan gagal ditambahkan');
+    }
+
+    return id;
+  }
+
+  // GET ALL NOTES
+  getNotes() {
+    return this._notes;
+  }
+
+  // GET NOTE BY ID
+  getNoteById(id) {
+    const note = this._notes.filter((n) => n.id === id)[0];
+    if (!note) {
+      throw new Error('Catatan tidak ditemukan');
+    }
+    return note;
+  }
+
+  // EDIT NOTE BY ID
+  editNoteById(id, { title, body, tags }) {
+    const index = this._notes.findIndex((note) => note.id === id);
+
+    if (index === -1) {
+      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+
+    const updatedAt = new Date().toISOString();
+
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+  }
+
+  // DELETE NOTE BY ID
+  deleteNoteById(id) {
+    const index = this._notes.findIndex((note) => note.id === id);
+    if (index === -1) {
+      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+    }
+    this._notes.splice(index, 1);
+  }
+}
+
+module.exports = NotesService;
